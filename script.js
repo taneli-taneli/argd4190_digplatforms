120a186d26864c53a8c9b623ad2dc5e57f6edf1a
taneli-taneli <tanelimajor1023@gmail.com>
2026-09-23 14:57:15 -0400

    push alien

---
+++ script.js
@@ -0,0 +1,46 @@
+
+<p>hello, world!</p>
+let number = 5
+let num2 = 10
+console.log(number + num2)
+let  name = 'taneli'
+let lastName = 'major'
+let student = { 
+name: 'taneli',
+lastName: 'major',
+major: 'graphic design',
+age: 24
+}
+
+console.log(student)
+let isDataLoaded = false
+//once the data is loaded, we can set the variable to true
+let nums = [1, 2, 3, 4, 5]
+console.log(nums[0])
+let firstName = 'taneli'
+firstName = "tilley"
+function addthreetonum (num) {
+    return num + 3
+}
+
+addthreetonum(5)
+console.log(addthreetonum(5))
+greetUser('taneli')
+function greetUser(name) {
+    console.log('hello, ' + name)
+}
+
+for (let i = 0; i < 4; i++) {
+    console.log(i)
+}
+let nums = ['taneli', 'tilley', 'grace']
+console.log(nums[0])
+
+let myelement = document.getElementById('my-element')
+myelement.innerHTML = 'hello, world!'
+myelement.style.color = 'red'
+myelement.style.fontSize = '24px'
+
+doSomething()
+
+hello, world!
